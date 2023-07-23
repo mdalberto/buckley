a97b7cf72d3de2e3710069e6acc65d9dab0f0b5f
mdalberto <mdal138@gmail.com>
2023-07-23 05:18:26 -0400

    adding assets and data for looping

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 // file that gets used by 11ty compiling
 
 module.exports = function(eleventyConfig){
+    eleventyConfig.addPassthroughCopy("src/assets/");
+    eleventyConfig.addWatchTarget("src/css/");
+
     return {
         dir: {
           input: 'src',
---
+++ src/_data/players.js
@@ -0,0 +1,6 @@
+module.exports = async function() {
+	return [
+		{"title": "test"},{"img":"/assets/img.gif"},
+		{"title": "test2"},{"img":"/assets/img2.gif"}
+	];
+};
